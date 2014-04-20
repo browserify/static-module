513edefd5bcd1c8ab3e2e7b7f02b501d807d8d0a
James Halliday <mail@substack.net>
2014-04-20 05:50:25 -0700

    failing assignment test

--- test/assign.js
+++ test/assign.js
@@ -21,6 +21,24 @@ test('assign', function (t) {
     }));
 });
 
+test('assign comma', function (t) {
+    t.plan(3);
+    
+    var expected = [ 12, 555 ];
+    var sm = staticModule({
+        beep: { x: 4, f: function (n) { return n * 111 } }
+    });
+    readStream('comma.js').pipe(sm).pipe(concat(function (body) {
+        t.equal(body.toString('utf8'),
+            'x=5;\n'
+            + 'console.log(4 * 3);\n'
+            + 'console.log(555);\n'
+        );
+        Function(['console'],body)({ log: log });
+        function log (msg) { t.equal(msg, expected.shift()) }
+    }));
+});
+
 function readStream (file) {
     return fs.createReadStream(path.join(__dirname, 'assign', file));
 }
