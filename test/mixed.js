07abbbdb95f0f378f745b151f6918b0d9d6c9fc0
James Halliday <mail@substack.net>
2014-04-20 04:26:46 -0700

    failing unmixed test

--- test/mixed.js
+++ test/mixed.js
@@ -5,7 +5,7 @@ var staticModule = require('../');
 var fs = require('fs');
 var path = require('path');
 
-test('mixed objects and streams', function (t) {
+test('mixed nested objects and streams', function (t) {
     t.plan(4);
     
     var expected = [ 12, 'oh hello\n', 555 ];
@@ -21,6 +21,31 @@ test('mixed objects and streams', function (t) {
         }
     }, { vars: { __dirname: path.join(__dirname, 'mixed') } });
     readStream('source.js').pipe(sm).pipe(concat(function (body) {
+        Function(['console'],body)({ log: log });
+        t.equal(
+            body.toString('utf8'),
+            '\nconsole.log(4 * 3);'
+            + '\nconsole.log("oh hello\\n");'
+            + '\nconsole.log(555);\n'
+        );
+        function log (msg) { t.equal(msg, expected.shift()) }
+    }));
+});
+
+test('mixed objects and streams', function (t) {
+    t.plan(4);
+    
+    var expected = [ 12, 'oh hello\n', 555 ];
+    var sm = staticModule({
+        beep: {
+            x: 4,
+            quote: function (file) {
+                return fs.createReadStream(file).pipe(quote());
+            },
+            f: function (n) { return n * 111 }
+        }
+    }, { vars: { __dirname: path.join(__dirname, 'mixed') } });
+    readStream('unmixed.js').pipe(sm).pipe(concat(function (body) {
         Function(['console'],body)({ log: log });
         t.equal(
             body.toString('utf8'),
