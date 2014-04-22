6d9723644ab44254a645d9d8cde6888e311db5cb
James Halliday <mail@substack.net>
2014-04-22 00:20:10 -0700

    another passing console test

--- test/log.js
+++ test/log.js
@@ -19,6 +19,27 @@ test('stream into a console.log', function (t) {
     }));
 });
 
+test('trickle stream into a console.log', function (t) {
+    t.plan(1);
+    var sm = staticModule({
+        beep: function () {
+            var q = quote();
+            var chunks = [ 'beep', ' boop', ' robots' ];
+            var iv = setInterval(function () {
+                if (chunks.length === 0) {
+                    clearInterval(iv);
+                    q.end();
+                }
+                else q.write(chunks.shift());
+            }, 10);
+            return q;
+        }
+    }, { vars: { __dirname: path.join(__dirname, 'brfs') } });
+    readStream('source.js').pipe(sm).pipe(concat(function (body) {
+        t.equal(body.toString('utf8'), 'console.log("beep boop robots");\n');
+    }));
+});
+
 function readStream (file) {
     return fs.createReadStream(path.join(__dirname, 'log', file));
 }
