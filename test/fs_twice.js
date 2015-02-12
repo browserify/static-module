ad751ad01fe33e0e5acf3df8d0c09239c3f322fa
James Halliday <mail@substack.net>
2015-02-11 18:15:02 -0800

    4x just to be sure

--- test/fs_twice.js
+++ test/fs_twice.js
@@ -42,6 +42,30 @@ test('fs.readFileSync twice in vars', function (t) {
     }));
 });
 
+test('fs.readFileSync 4x', function (t) {
+    var expected = [
+        'EXTERMINATE\n', 'beep boop\n', 'EXTERMINATE\n', 'beep boop\n'
+    ];
+    t.plan(expected.length + 1);
+    var sm = staticModule({
+        fs: { readFileSync: readFileSync }
+    }, { vars: { __dirname: __dirname + '/fs_twice' } });
+    readStream('4x.js').pipe(sm).pipe(concat(function (body) {
+        t.equal(body.toString('utf8').trim(),
+            'var a = "EXTERMINATE\\n";\n'
+            + 'var b = "beep boop\\n";\n'
+            + 'var c = "EXTERMINATE\\n";\n'
+            + 'var d = "beep boop\\n";\n'
+            + 'console.log(a);\n'
+            + 'console.log(b);\n'
+            + 'console.log(c);\n'
+            + 'console.log(d);'
+        );
+        Function(['console'],body)({ log: log });
+        function log (msg) { t.equal(msg, expected.shift()) }
+    }));
+});
+
 function readStream (file) {
     return fs.createReadStream(path.join(__dirname, 'fs_twice', file));
 }
